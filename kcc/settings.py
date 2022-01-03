89081a2352fd8d54db6f5bba397bdaac8c8f5802
Mahesh Jadhav <omkar28399@gmail.com>
2022-01-03 21:32:50 +0530

    whitenoise dependency added

--- kcc/settings.py
+++ kcc/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
