75f1c8eb5565cd347a462478053720579489f149
Mahesh Jadhav <omkar28399@gmail.com>
2022-01-03 21:20:27 +0530

    settings file updated

--- kcc/urls.py
+++ kcc/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from classes import views
 
+from django.conf.urls.static import static
+from kcc import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="index"),
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
